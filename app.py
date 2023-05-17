c2d3bf03f84285e7ceacfca38fff946dde0b4d64
Ahmad Hatahet <ahmad.hatahet@hotmail.com>
2023-05-17 19:54:13 +0200

    add convert_to_text

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import time
 from log_config import init_logger
 import logging
 from capture_audio import start_recording
+from audio_to_text import start_transcript
 
 # initaite logger
 init_logger()
@@ -13,11 +14,15 @@ def record_audio():
     return start_recording(
         recording_duration=10,
         interval_duration=0,
-        total_duration=2
+        total_duration=0.5
     )
 
-def convert_to_text(audio_file):
+def convert_to_text(file_name):
     print('Converting to text ... !')
+
+    if file_name is None: raise ValueError('No file name was passed!')
+
+    return start_transcript(file_name)
 
 # Streamlit app code
 def main():
@@ -25,17 +30,23 @@ def main():
 
     # Record audio
     st.header("Record Audio")
+    file_name = None
     if st.button("Start Recording"):
-        record_audio()
+        file_name = record_audio()
         st.success("Recording finished!")
 
     # Convert audio to text
     st.header("Convert to Text")
-    audio_file = st.file_uploader("Upload an audio file")
-    if audio_file is not None:
-        if st.button("Convert"):
-            text = convert_to_text(audio_file)
-            st.success("Text: {}".format(text))
+    if st.button("Convert"):
+
+        try:
+
+            text_files_path = convert_to_text(file_name)
+            st.success("Text: {}".format(str(text_files_path)))
+
+        except ValueError:
+
+            st.error('No file name was passed!')
 
 if __name__ == "__main__":
     main()
