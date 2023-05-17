6df15570ad960f0b1b92db06d00edc9cba0d6ed1
Ahmad Hatahet <ahmad.hatahet@hotmail.com>
2023-05-17 19:34:17 +0200

    add logger and move api key to env variable

--- summarize_bot.py
+++ summarize_bot.py
@@ -4,16 +4,35 @@ from openai.error import RateLimitError
 from time import sleep
 from log_config import init_logger
 import logging
+from dotenv import load_dotenv
+import os
 
 # initaite logger
 init_logger()
-logger = logging.getLogger('audio-to-text')
+logger = logging.getLogger('summrize')
 
+# get base folder
+base = Path(__file__).parent
+path_text_files = base / 'text_files' / '2023-05-17_18-37'
 
+# load env variables
+load_dotenv(base / '.env')
 
-openai.api_key = "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"
+# open ai api key
+openai.api_key = os.environ['openai_api_key']
+
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
+    """
+    Send a request and return the response message.
+
+    Args:
+        prompt (str): the prompt text
+        model (str, optional): Name of the model from Open AI. Defaults to "gpt-3.5-turbo".
+
+    Returns:
+        str: The model response.
+    """
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model=model,
@@ -23,13 +42,9 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     return response.choices[0].message["content"]
 
 
-# get base folder
-base = Path(__file__).parent
-path_text_files = base / 'text_files' / '2023-05-17_18-37'
-
-text_corpus = []
 
 # read all texts
+text_corpus = []
 for text_file in path_text_files.iterdir():
 
     # append to list
