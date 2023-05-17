1572225813bfff67bb80d7b0e2a2da6747b05133
Ahmad Hatahet <ahmad.hatahet@hotmail.com>
2023-05-17 20:12:30 +0200

    add vars to state and integrate with summarize func

--- app.py
+++ app.py
@@ -4,17 +4,23 @@ from log_config import init_logger
 import logging
 from capture_audio import start_recording
 from audio_to_text import start_transcript
+from summarize_bot import summarize
 
 # initaite logger
 init_logger()
 logger = logging.getLogger('audio-to-text')
+
+keys = ['file_name', 'text_files_path']
+for k in keys:
+    if k not in st.session_state:
+        st.session_state[k] = None
 
 def record_audio():
     print('Recording ... !')
     return start_recording(
         recording_duration=10,
         interval_duration=0,
-        total_duration=0.5
+        total_duration=1
     )
 
 def convert_to_text(file_name):
@@ -28,12 +34,19 @@ def convert_to_text(file_name):
 def main():
     st.title("Audio Recording and Conversion")
 
-    # Record audio
-    st.header("Record Audio")
-    file_name = None
-    if st.button("Start Recording"):
-        file_name = record_audio()
-        st.success("Recording finished!")
+    # # Record audio
+    # st.header("Record Audio")
+    # if st.button("Start Recording"):
+
+    #     try:
+
+    #         st.session_state['file_name'] = record_audio()
+    #         st.success("Recording finished!")
+
+    #     except:
+    #         st.warning('Recording was interrupted!')
+
+    # st.info(file_name)
 
     # Convert audio to text
     st.header("Convert to Text")
@@ -41,12 +54,17 @@ def main():
 
         try:
 
-            text_files_path = convert_to_text(file_name)
-            st.success("Text: {}".format(str(text_files_path)))
+            st.session_state['text_files_path'] = convert_to_text('2023-05-17_20-00')
+            st.success("Text: {}".format(str(st.session_state['text_files_path'])))
 
         except ValueError:
 
             st.error('No file name was passed!')
+
+
+    st.header("Summarize Text:")
+    if st.button("Summarize"):
+        st.write(summarize(st.session_state['text_files_path']))
 
 if __name__ == "__main__":
     main()
