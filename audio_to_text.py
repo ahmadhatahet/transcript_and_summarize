1fa86452cb907988823d896e89057b06033714e6
Ahmad Hatahet <ahmad.hatahet@hotmail.com>
2023-05-17 19:53:53 +0200

    create start_transcript function

--- audio_to_text.py
+++ audio_to_text.py
@@ -10,34 +10,37 @@ init_logger()
 logger = logging.getLogger('audio-to-text')
 
 
-# get base folder
-base = Path(__file__).parent
-path_audio_files = base / 'audio_files' / '2023-05-17_18-37'
+def start_transcript(file_name):
+    # get base folder
+    base = Path(__file__).parent
+    path_audio_files = base / 'audio_files' / file_name
 
-# folder to save text files
-path_text_files = base / 'text_files' / '2023-05-17_18-37'
-path_text_files.mkdir(exist_ok=True, parents=True)
+    # folder to save text files
+    path_text_files = base / 'text_files' / file_name
+    path_text_files.mkdir(exist_ok=True, parents=True)
 
-# Initialize the recognizer
-r = sr.Recognizer()
+    # Initialize the recognizer
+    r = sr.Recognizer()
 
-# read audio file
-for audio_file in path_audio_files.iterdir():
+    # read audio file
+    for audio_file in path_audio_files.iterdir():
 
-    # Load the audio file
-    with sr.AudioFile(audio_file.__str__() ) as source:
-        audio = r.record(source)
+        # Load the audio file
+        with sr.AudioFile(audio_file.__str__() ) as source:
+            audio = r.record(source)
 
-    # Convert audio to text
-    text = r.recognize_google(audio, language="en")
+        # Convert audio to text
+        text = r.recognize_google(audio, language="en")
 
-    # text = arabic_reshaper.reshape(text)
-    # Print the recognized text
-    # print(text[::-1])
+        # text = arabic_reshaper.reshape(text)
+        # Print the recognized text
+        # print(text[::-1])
 
-    # create text file
-    file = path_text_files / f'{audio_file.name}.txt'
-    file.touch()
+        # create text file
+        file = path_text_files / f'{audio_file.name}.txt'
+        file.touch()
 
-    # save text
-    file.write_text(text, 'utf-8')
+        # save text
+        file.write_text(text, 'utf-8')
+
+        return path_text_files
